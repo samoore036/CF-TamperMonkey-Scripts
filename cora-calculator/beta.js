81733bd99fd1fcec91915d01a4e286f395b9bbef
samoore036 <104536361+samoore036@users.noreply.github.com>
2023-03-30 15:22:06 -0700

    Updated to show nonsortables only
    Added third api to pull all FC list. Used this to display nonsortables (both TNS and AR) only.

--- cora-calculator/beta.js
+++ cora-calculator/beta.js
@@ -9,7 +9,14 @@
 // @match        https://outboundflow-iad.amazon.com/*
 // @grant        GM.xmlHttpRequest
 // @connect      trb-na.corp.amazon.com
+// @connect      ecft.fulfillment.a2z.com
 // ==/UserScript==
+
+const alertDiv = document.getElementsByClassName('node')[0].querySelectorAll('div')[1].querySelectorAll('div')[0];
+alertDiv.textContent = 'Loading...';
+styleParent();
+loadingStyles();
+
 
 // all of the risk information for trbs
 const trbData = new Promise(function(resolve) {
@@ -41,23 +48,80 @@ const processingTimes = new Promise(function(resolve) {
     })
 });
 
-Promise.all([trbData, processingTimes]).then(data => loadScript(data));
+// pull fc list to filter for non-sortables
+const fcList = new Promise(function(resolve) {
+    GM.xmlHttpRequest({
+        method: 'GET',
+        url: 'https://ecft.fulfillment.a2z.com/api/NA/fc-na',
+        onreadystatechange: function(response) {
+            if (response.readyState === 4 && response.status === 200) {
+                resolve(this.response);
+            } else {
+                console.log('fc list pull unsuccessful');
+            }
+        }
+    })
+});
+
+Promise.all([trbData, processingTimes, fcList]).then(data => loadScript(data));
 
 function loadScript(data) {
-    if (!data[0] || !data[1]) {
-        function displayError();
+    // this div is useless so trb display/errors will go here
+    const divToReplace = document.getElementsByClassName('node')[0].querySelectorAll('div')[1].querySelectorAll('div')[0];
+    divToReplace.textContent = 'TRB status';
+    let fc = document.URL.split('/')[3];
+
+    if (!data[0] || !data[1] || !data[2]) {
+        alertDiv.textContent = 'Content failed to load. Please refresh to try again.';
+        return;
     }
 
-    let fc = document.URL.split('/')[3];
+    console.log(JSON.parse(data[2]));
+
+    const nonsortables = JSON.parse(data[2])
+                            .filter(site => site.fc_type.includes('Non-Sortable'))
+                            .map(site => site.fc);
+
+
+    loadedStyles();
 
     let apiData = data[0].replaceAll('[', '').replaceAll(']', '').split('{');
     const trbAllData = apiData.filter(string => string !== '').map(stringData => jsonify(stringData));
     const processTimes = JSON.parse(data[1]);
     
-    //the api returns trbs for all ship options but the only one we are concerned with is default shipping 
+    /*
+    the api returns trbs for all ship options but the only one we are concerned with is default shipping 
+    a site is on trb if its time to cpt is greater than the process time
+    only non sortables are displayed
+    */
     const actualTrbs = trbAllData
                         .filter(trb => trb.shipOption === 'default')
-                        .filter(trb => isOnTrb(trb.timeToCpt, processTimes[trb.fc].default));
+                        .filter(trb => isOnTrb(trb.timeToCpt, processTimes[trb.fc].default))
+                        .filter(trb => isNonsortable(trb.fc))
+                        .sort((a,b) => a.fc > b.fc);
+
+    alertDiv.appendChild(createTrbAlertDiv(actualTrbs));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     /**************
      Helper Methods
@@ -77,7 +141,7 @@ function loadScript(data) {
         let timeToCpt = beautifyString(strArr[9].split(':')[1]);   
         let risk = beautifyString(strArr[8].split(':')[1]);
         let shipOption = beautifyString(strArr[10].split(':')[1]);
-        let packGroup = beautifyString(strArr[11].split(':')[1]);
+        let packGroup = beautifyString(strArr[11].split(':')[1]).replaceAll('}', '');
         return {
             fc: fc,
             cpt: cpt,
@@ -90,6 +154,9 @@ function loadScript(data) {
 
     // the api is a plain text file that has escape characters and quotes that need to be replaced
     function beautifyString(str) {
+        if (!str) {
+            return;
+        }
         return str.replaceAll('"', '').replaceAll('\\', '');
     }
 
@@ -97,9 +164,60 @@ function loadScript(data) {
     function isOnTrb(timeToCpt, processingTime) {
         return timeToCpt > processingTime;
     }
-
-    function displayError() {
-        const divToReplace = document.getElementsByClassName('node')[0].querySelectorAll('div')[1].querySelectorAll('div')[0];
-        divToReplace.textContent = '';
+    
+    function isNonsortable(fc) {
+        for (let i = 0; i < nonsortables.length; i++) {
+            if (fc === nonsortables[i]) {
+                return true;
+            }
+        }
+        return false;
     }
+
+    /**************
+     Helper Methods
+    **************/
+   function createTrbAlertDiv(trbArray) {
+    const div = document.createElement('div');
+    trbArray.forEach(trb => div.appendChild(createStatusAlert(trb)));
+    return div;
+   }
+
+   function createStatusAlert(trb) {
+        const div = document.createElement('div');
+        div.classList.add('trb-status');
+        div.textContent = `${trb.fc} | ${trb.cpt} | ${trb.packGroup}`;
+        return div;
+   }
+}
+
+/*****
+Styles
+*****/
+function styleParent() {
+    const parent = alertDiv.parentElement;
+    parent.style.cssText += parentElementStyles();
+}
+
+function parentElementStyles() {
+    return `
+        display: flex;
+        flex-direction: column;
+    `
+}
+
+function loadingStyles() {
+    alertDiv.style.cssText += `
+        align-self: center;
+        padding: 2rem;
+        font-size: 3rem;
+        font-weight: bold;
+    `
+}
+
+function loadedStyles() {
+    alertDiv.style.cssText += `
+        font-size: 1rem;
+        font-weight: 400;
+    `
 }
