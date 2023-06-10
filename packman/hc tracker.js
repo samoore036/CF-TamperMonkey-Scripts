9d2fc7bcd3b28bc4f3232192e66d45e86dce72a1
samoore036 <104536361+samoore036@users.noreply.github.com>
2023-06-10 13:10:20 -0700

    added basic pack tracker
    Added div that displays summarized pack hcs with preliminary filters

--- packman/hc tracker.js
+++ packman/hc tracker.js
@@ -3,7 +3,7 @@
 // @updateURL    https://github.com/samoore036/CF-TamperMonkey-Scripts/blob/main/dwell-callouts/cpt-dwells.js
 // @downloadURL  https://github.com/samoore036/CF-TamperMonkey-Scripts/blob/main/dwell-callouts/cpt-dwells.js
 // @namespace    https://github.com/samoore036/CF-TamperMonkey-Scripts
-// @version      1.0
+// @version      1.0.2
 // @description  Display all pick settings, including hcs, and pack hcs
 // @author       mooshahe
 // @match        https://insights.prod-na.pack.aft.a2z.com/packman/recent?fc=*
@@ -63,9 +63,12 @@ const batchData = new Promise(function(resolve) {
     })
 })
 
+// packman grabs it's own apis and loads dom, so must wait for these to load before executing script
 //after data is retrieved for set process paths, get active batch data, then wait until DOM is loaded then execute script
 Promise.all([activeData, setData, batchData]).then((data) => {
-    loadScript(data);
+    setTimeout(() => {
+        loadScript(data);
+    }, 4000);
 })
 
 function loadScript(data) {
@@ -82,21 +85,110 @@ function loadScript(data) {
         localStorage.setItem('time-pref', '15');
     }
 
-    makePackerObjects();
+    let singles = 0;
+    let multis = 0;
+    let slap = 0;
+    let handTape = 0;
+    let bod = 0;
+
+    parseData(getPackData());
+
+    const parentDiv = document.getElementsByClassName('mat-tab-body-content')[0];
+    const div = document.createElement('div');
+    div.textContent = `singles: ${singles}, multis: ${multis}, slap: ${slap}, bod: ${bod} `;
+    if (handTape > 0) {
+        div.textContent += `handtape: ${handTape}`;
+    }
+    parentDiv.prepend(div);
 
     // there is no get api so must iterate over the DOM and create an object with each packer's information to then parse
     // default timeFrame is 15 minutes, but user can change this and store as a cookie
-    function makePackerObjects() {
-        const timePref = parseInt(localStorage.getItem('time-pref'));
+
+    // main methods
+    function getPackData() {
+        const packData = [];
 
         const rows = Array.from(document.querySelector('tbody').querySelectorAll('tr'));
         for (let i = 0; i < rows.length; i++) {
             // filter out if start time is greater than time pref
             const startTime = rows[i].querySelectorAll('td')[2].textContent;
             if (!isWithinTimePref(startTime)) {
+                console.log('not in time pref');
                 continue;
             } else {
-                
+                console.log('within time pref');
+                packData.push({
+                    workStation: rows[i].querySelectorAll('td')[5].textContent.trim(),
+                    packMode: rows[i].querySelectorAll('td')[6].textContent.trim(),
+                    processPath: rows[i].querySelectorAll('td')[7].textContent.trim()
+                })
+
+            }
+        }
+
+        return packData;
+    }
+
+    function parseData(packData) {
+        for (let i = 0; i < packData.length; i++) {
+            // if ((packData[i].processPath.includes('Single') || packData[i].processPath.includes('HOV')) && (packData[i].workStation.includes('wsPack') || packData[i].workStation.includes('wsGW'))) {
+            //     singles++;
+            //     continue;
+            // }
+            // if (packData[i].packMode === 'rebin') {
+            //     multis++;
+            //     continue;
+            // }
+            // if (packData[i].workStation.match('ws[0-9]+') || (packData[i].workStation.includes('BOD') && packData[i].processPath.includes('NonCon'))) {
+            //     slap++;
+            //     continue;
+            // }
+            // if (packData[i].processPath.includes('HandTape') && packData[i].workStation.includes('wsBOD')) {
+            //     handTape++;
+            //     continue;
+            // }
+            // if (packData[i].workStation.includes('wsBOD')) {
+            //     bod++;
+            // }
+            const { processPath, packMode, workStation } = packData[i];
+            // skip psolve
+            if (workStation.includes('POPS' || workStation.includes('pops'))) {
+                continue;
+            }
+            if (processPath.includes('MCF')) {
+                if (packMode.includes('singles_slam')) {
+                    slap++;
+                    continue;
+                }
+                if (workStation.includes('BOD')) {
+                    bod++;
+                    continue;
+                }
+                singles++;
+            }
+            if (processPath.includes('Single') && !processPath.includes('BOD') && !processPath.includes('NonCon') && !processPath.includes('SIOC') && !processPath.includes('HandTape')) {
+                singles++;
+            }
+            if (processPath.includes('Multi')) {
+                multis++;
+            }
+            if (processPath.includes('NonCon')) {
+                slap++;
+            }
+            if (processPath.includes('HandTape')) {
+                handTape++;
+            }
+            if (processPath.includes('BOD')) {
+                bod++;
+            }
+            if (processPath.includes('HOV')) {
+                if (packMode.includes('singles')) {
+                    singles++;
+                } else if (packMode.includes('singles_slam') && workStation.includes('BOD')) {
+                    bod++;
+                } else {
+                    slap++;
+                }
             }
         }
     }
@@ -106,12 +198,22 @@ function loadScript(data) {
     // helper methods
     function isWithinTimePref(startTime) {
         // convert to date object for comparison
-        console.log(startTime.trim().split(' '));
         const pieces = startTime.trim().split(' ');
         const year = pieces[2];
         const month = pieces[0];
         const day = pieces[1];
+        const am = pieces[4] === 'AM' ? true : false;
+        const timeString = pieces[3].split(':');
+        const hour = am ? timeString[0] : parseInt(timeString[0]) + 12;
+        const minute = timeString[1];
+        const second = timeString[2];
         
+        const startTimeObj = new Date(`${month} ${day}, ${year} ${hour}:${minute}:${second}`);
+        
+        let timeNowWithTimePref = new Date();
+        let newMinutes = timeNowWithTimePref.getMinutes() - localStorage.getItem('time-pref');
+        timeNowWithTimePref.setMinutes(newMinutes);
 
+        return timeNowWithTimePref > startTimeObj ? false : true;
     }
 }
