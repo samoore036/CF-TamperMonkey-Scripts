ef90f20956ecb82b10ad10eec563b315044d9812
samoore036 <104536361+samoore036@users.noreply.github.com>
2023-06-10 15:19:42 -0700

    added pack table
    Created the master div, pick div, pack div and pack table. Pack data is hooked up to pack table

--- packman/hc tracker.js
+++ packman/hc tracker.js
@@ -3,7 +3,7 @@
 // @updateURL    https://github.com/samoore036/CF-TamperMonkey-Scripts/blob/main/dwell-callouts/cpt-dwells.js
 // @downloadURL  https://github.com/samoore036/CF-TamperMonkey-Scripts/blob/main/dwell-callouts/cpt-dwells.js
 // @namespace    https://github.com/samoore036/CF-TamperMonkey-Scripts
-// @version      1.0.2
+// @version      1.1.0
 // @description  Display all pick settings, including hcs, and pack hcs
 // @author       mooshahe
 // @match        https://insights.prod-na.pack.aft.a2z.com/packman/recent?fc=*
@@ -85,21 +85,18 @@ function loadScript(data) {
         localStorage.setItem('time-pref', '15');
     }
 
-    let singles = 0;
-    let multis = 0;
-    let slap = 0;
-    let handTape = 0;
-    let bod = 0;
+    const packHeadcounts = {
+        singles: 0,
+        multis: 0,
+        bod: 0,
+        slap: 0,
+        handTape: 0
+    }
 
     parseData(getPackData());
 
-    const parentDiv = document.getElementsByClassName('mat-tab-body-content')[0];
-    const div = document.createElement('div');
-    div.textContent = `singles: ${singles}, multis: ${multis}, slap: ${slap}, bod: ${bod} `;
-    if (handTape > 0) {
-        div.textContent += `handtape: ${handTape}`;
-    }
-    parentDiv.prepend(div);
+    makeDivs();
+    loadPackTableData();
 
     // there is no get api so must iterate over the DOM and create an object with each packer's information to then parse
     // default timeFrame is 15 minutes, but user can change this and store as a cookie
@@ -157,40 +154,57 @@ function loadScript(data) {
             }
             if (processPath.includes('MCF')) {
                 if (packMode.includes('singles_slam')) {
-                    slap++;
+                    packHeadcounts.slap++;
                     continue;
                 }
                 if (workStation.includes('BOD')) {
-                    bod++;
+                    packHeadcounts.bod++;
                     continue;
                 }
-                singles++;
+                packHeadcounts.singles++;
             }
             if (processPath.includes('Single') && !processPath.includes('BOD') && !processPath.includes('NonCon') && !processPath.includes('SIOC') && !processPath.includes('HandTape')) {
-                singles++;
+                packHeadcounts.singles++;
             }
             if (processPath.includes('Multi')) {
-                multis++;
+                packHeadcounts.multis++;
             }
             if (processPath.includes('NonCon')) {
-                slap++;
+                packHeadcounts.slap++;
             }
             if (processPath.includes('HandTape')) {
-                handTape++;
+                packHeadcounts.handTape++;
             }
             if (processPath.includes('BOD')) {
-                bod++;
+                packHeadcounts.bod++;
             }
             if (processPath.includes('HOV')) {
                 if (packMode.includes('singles')) {
-                    singles++;
+                    packHeadcounts.singles++;
                 } else if (packMode.includes('singles_slam') && workStation.includes('BOD')) {
-                    bod++;
+                    packHeadcounts.bod++;
                 } else {
-                    slap++;
+                    packHeadcounts.slap++;
                 }
             }
         }
+    }
+
+    function makeDivs() {
+        const parentDiv = document.getElementsByClassName('mat-tab-body-content')[0];
+        const masterDiv = makeMasterDiv();
+        parentDiv.appendChild(masterDiv);
+
+        const pickDiv = makePickDiv();
+        masterDiv.appendChild(pickDiv);
+
+        const packDiv = makePackDiv();
+        const packTable = makePackTable();
+
+        packDiv.appendChild(packTable);
+        masterDiv.appendChild(packTable);
+
+        parentDiv.prepend(masterDiv);
     }
 
 
@@ -216,4 +230,153 @@ function loadScript(data) {
 
         return timeNowWithTimePref > startTimeObj ? false : true;
     }
+
+    function makeMasterDiv() {
+        const masterDiv = document.createElement('div');
+
+        return masterDiv;
+    }
+
+    function makePickDiv() {
+        const pickDiv = document.createElement('div');
+
+        return pickDiv;
+    }
+
+    function makePackDiv() {
+        const packDiv = document.createElement('div');
+
+        return packDiv;
+    }
+
+    function makePackTable() {
+        const packTable = document.createElement('table');
+        packTable.setAttribute('id', 'pack-table');
+        const titleRow = document.createElement('tr');
+        titleRow.style.fontSize = '2rem';
+        titleRow.style.color = 'white';
+        titleRow.style.backgroundColor = '#3b82f6';
+        titleRow.style.border = '1px solid black';
+        titleRow.style.borderBottom = 'none';
+        const titleHeader = document.createElement('th');
+        titleHeader.classList.add('table-header');
+        titleHeader.textContent = `Pack hcs in the last ${localStorage.getItem('time-pref')} minutes`;
+        titleHeader.colSpan = '11';
+        titleRow.appendChild(titleHeader);
+        packTable.appendChild(titleRow);
+
+        const categoriesRow = document.createElement('tr');
+        categoriesRow.style.fontSize = '1.5rem';
+        categoriesRow.style.color = 'white';
+        categoriesRow.style.backgroundColor = '#3b82f6';
+        categoriesRow.style.borderLeft = '1px solid black';
+        categoriesRow.style.borderRight = '1px solid black';
+
+        const td1 = makeHeaderTd('Pack Group');
+        categoriesRow.appendChild(td1);
+        const td2 = makeHeaderTd('Active Packers');
+        td2.title = 'Detected using the below pack data';
+        td2.style.cursor = 'help';
+        categoriesRow.appendChild(td2);
+        const td3 = makeHeaderTd('Planned Packers');
+        categoriesRow.appendChild(td3);
+        const td4 = makeHeaderTd('Delta');
+        categoriesRow.appendChild(td4);
+        const td5 = makeHeaderTd('Deviation');
+        categoriesRow.appendChild(td5);
+        packTable.appendChild(categoriesRow);
+
+        return packTable;
+    }
+
+    function loadPackTableData() {
+        const packTable = document.getElementById('pack-table');
+        for (const [key, value] of Object.entries(packHeadcounts)) {
+            if (!value || value === 0) {
+                continue;
+            }
+            const tr = makePackTableRow(key, value);
+            packTable.appendChild(tr);
+        }
+    }
+
+    function makePackTableRow(packGroup, hc) {
+        const tr = document.createElement('tr');
+        const name = makeTd(packGroup);
+        tr.appendChild(name);
+
+        const activePackersTd = makeTd(hc);
+        tr.appendChild(activePackersTd);
+
+        let plannedPackers = 0;
+        if (localStorage.getItem(`${fc}-${packGroup}`)) {
+            plannedPackers = localStorage.getItem(`${fc}-${packGroup}`);
+        }
+
+        const plannedPackersTd = makeInputTd(packGroup);
+        tr.appendChild(plannedPackersTd);
+
+        const delta = hc - plannedPackers;
+        const deltaTd = makeTd(delta);
+        deltaTd.style.backgroundColor = delta === 0 ? '#22c55e' : '#f87171';
+        tr.appendChild(deltaTd);
+
+        const deviationPercent = getDeviationPercent(hc, plannedPackers);
+        const deviationTd = makeTd(deviationPercent);
+        tr.appendChild(deviationTd);
+
+        return tr;
+    }
+
+    function makeTd(data) {
+        const td = document.createElement('td');
+        td.textContent = data;
+
+        return td;
+    }
+
+    // use local storage to load in saved plan pack hc so don't have to reenter upon refresh
+    // if no local storage for that pack group exists, use 0
+    // when user changes the input, automatically save new input in local storage
+    // the key is saved as fc-packGroup as leads run multiple sites
+    function makeInputTd(packGroup) {
+        const td = document.createElement('td');
+        const input = document.createElement('input');
+        if (localStorage.getItem(`${fc}-${packGroup}`)) {
+            input.value = localStorage.getItem(`${fc}-${packGroup}`);
+        } else {
+            input.value = 0;
+        }
+
+        input.addEventListener('change', (e) => {
+            localStorage.setItem(`${fc}-${packGroup}`, e.target.value);
+        })
+        td.appendChild(input);
+
+        return td;
+    }
+
+    function makeHeaderTd(str) {
+        const td = document.createElement('td');
+        td.classList.add('header-td');
+        td.textContent = str;
+        return td;
+    }
+
+    function getDeviationPercent(activePackers, plannedPackers) {
+        if (activePackers == 0) {
+            return '100%';
+        }
+        const deviationPercent = ((plannedPackers - activePackers) / plannedPackers) * 100 * -1;
+        return `${deviationPercent > 0 ? '+' : ''}${deviationPercent.toFixed(0)}%`;
+    }
 }
+
+
+/* to dos
+make div table for pack hcs
+display pick table
+add flags if noncon processed in BOD or vice versa
+
+
+*/
